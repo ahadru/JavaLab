88a0f90e65f6c7ec72e51f6f2c4be1ccc8834f47
Ahad <ahadsheikh1999@gmail.com>
2018-09-16 14:16:06 +0600

    Using Random Class

--- Random.java
+++ Random.java
@@ -1,6 +1,6 @@
 public class Random{
     public static void main(String[] args){
-        int random = (int )(Math.random() * 50 + 1);
+        int random = (int )(Math.random() * 10 + 1);
 
         System.out.println(random);
     }
