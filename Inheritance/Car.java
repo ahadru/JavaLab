f386e3452df49bdcd5b62a314861f3fc54b0ac4f
Abdul ahad <ahadsheikh1999@gmail.com>
2018-09-21 02:21:00 +0600

    Learning some inheritance in Java

---
+++ Inheritance/Car.java
@@ -0,0 +1,11 @@
+public class Car extends Vehicle{
+    private int cars;
+    Car(int c){
+        super(c);
+        cars = c;
+    }
+    aha
+    public int numberOfCars(){
+        return cars;
+    }
+}
--- Inheritance/Vehicle.java
+++ Inheritance/Vehicle.java
@@ -1,3 +1,10 @@
-public class Vehicle{
-    
+public abstract class Vehicle{
+    private int vehicles = 0;
+    Vehicle(int n){
+        vehicles = vehicles + n;
+    }
+    public abstract int brand();
+    public int numberOfVehicle(){
+        return vehicles;
+    }
 }
