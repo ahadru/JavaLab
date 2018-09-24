c983299fda02edb9a18c0608d5cddb940d5dc5fc
Ahad <ahadsheikh1999@gmail.com>
2018-09-24 13:26:14 +0600

    Little more on thread

--- multi-thread/Main.java
+++ multi-thread/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        // MyGreetings r1 = new MyGreetings(", Hello");
-        // MyGreetings r2 = new MyGreetings(", Good Morning");
-        Thread t1 = new MyGreetings(r1);
-        Thread t2 = new MyGreetings(r2);
+        Runnable r1 = new MyGreetings(", Hello");
+        Runnable r2 = new MyGreetings(", Good Morning");
+        Thread t1 = new Thread(r1);
+        Thread t2 = new Thread(r2);
+        t1.interrupt();
         t1.start();
         t2.start();
     }
--- multi-thread/MyGreetings.java
+++ multi-thread/MyGreetings.java
@@ -10,7 +10,7 @@ public class MyGreetings extends Thread{
 
     public void run() {
         try {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 10 && !Thread.interrupted(); i++) {
                 Date now = new Date();
                 System.out.println(now + " " + greetings);
                 Thread.sleep(DELAY);
