30097a11d9aae661ae0989f39814b06210dc1533
Ahad <ahadsheikh1999@gmail.com>
2018-11-25 16:41:37 +0600

    Exam

---
+++ Eclipse/TicketSeller/src/test/Testing.java
@@ -0,0 +1,30 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import main.TS;
+
+class Testing {
+	TS ob = new TS();
+	
+	@Test
+	void testOperateBuy() {
+		assertEquals(true, ob.OperateBuy(3), "answer should to be true");
+	}
+
+	@Test
+	void testReset() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSoldTickets() {
+		assertEquals(10, ob.getSoldTickets(), "buyed ticket");
+	}
+
+	@Test
+	void testGetBuyer() {
+		assertEquals(1, ob.getBuyer(), "buyer");
+	}
+}
